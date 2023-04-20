1eb7d456116238bf42cc805dcad46d5fcab09eb3
GabrielHenri12 <gabrielhenri2002@gmail.com>
2023-04-20 20:19:03 -0300

    removendo arquivos de configs

--- nyc.config.js
+++ nyc.config.js
@@ -4,8 +4,6 @@ module.exports = {
     exclude: [
       'src/tests',
       'src/database/config',
-      'src/database/migrations',
-      'src/database/seeders'
     ],
     include: ['src/**/*.ts']
   };
